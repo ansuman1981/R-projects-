471895d9ef03b3fd7ff0ba8d2c42fef7dd548fa8
ansuman1981 <ansuman.jaiswal@my.jcu.edu.au>
2026-03-16 17:41:32 +0800

    function 2 arguments done

--- 2.1 function in R.R
+++ 2.1 function in R.R
@@ -12,3 +12,19 @@ func1 <- function(x)
   # some places teach return is not necessary; but let's retain it for clarity
 }
 func1(5)
+
+# Function with 2 arguments
+func2 <- function(x,y)
+{
+  z <- 0.8*x + 0.5 *y
+  return (z)
+}
+func2(5,2)
+
+# Write a simple function that concatenate your name with 'student in JCU'
+func3 <- function(x){
+  z <- paste(x," is lecturer at JCU")
+  return (z)
+}
+func3("Eric")
+
