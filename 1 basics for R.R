cc55a542d6b91cec1e8015c0747df39fa79df623
ansuman1981 <ansuman.jaiswal@my.jcu.edu.au>
2026-03-16 17:26:49 +0800

    basic for R install R helpful command

---
+++ 1 basics for R.R
@@ -0,0 +1,18 @@
+# 1 basic of R
+# 1.	Download and install R 
+# 2.	Download and install RStudio 
+# 3.  Run RMarkdown as for a report
+# https://rmarkdown.rstudio.com/authoring_basics.html
+# 4. Run ShinyLab and set up account in 
+# https://www.shinyapps.io/
+# 
+###################################################
+
+# 1.	Some helpful commands
+# print() and "#" and ls() and rm() and "ctrl+L" (to clear)
+var1 = 1
+print(var1)
+ls()
+rm(var1)
+help()
+?rm()
